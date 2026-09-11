2f7eb4b495e2c199041374214a421d1e62603444
juanchizambo-max <juanchizambo@gmail.com>
2026-09-11 11:46:51 +0200

    Add user input for robot preference and countdown
    Fix syntax errors and improve user interaction.

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-print("hello test")
+answer = input("Hello user do you like robots") 
+if answer = yes:
+  robot = input("Cool wich ones? a: War robots;b: ai assistents; C:car robots") 
+if robot = a:
+  print("System override... ... ...")
+  ("                    ")
+  timer = 60
+  for 60 in range(0,60)
+  print("Exploding in", timer,"seconds")
+  timer = timer-1 
