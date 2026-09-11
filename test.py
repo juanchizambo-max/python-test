fb4173a0ab220084556afa78124b1a429e392406
juanchizambo-max <juanchizambo@gmail.com>
2026-09-11 11:51:51 +0200

    Correct input conditions and countdown timer
    Fix syntax errors and improve countdown logic.

--- test.py
+++ test.py
@@ -1,10 +1,10 @@
 answer = input("Hello user do you like robots") 
-if answer = yes:
+if answer == "yes":
   robot = input("Cool wich ones? a: War robots;b: ai assistents; C:car robots") 
-if robot = a:
+if robot == a:
   print("System override... ... ...")
   ("                    ")
   timer = 60
-  for 60 in range(0,60)
+  for timer in range(0,60,-1)
   print("Exploding in", timer,"seconds")
-  timer = timer-1 
+  time.sleep(1)
